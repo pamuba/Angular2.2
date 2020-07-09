69c36e93e315bb11bc7584a6b684772190bb87c7
pamuba <pamuba@users.noreply.github.com>
2020-07-09 20:37:10 +0530

    46

--- app/department-detail.component.js
+++ app/department-detail.component.js
@@ -5,16 +5,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var DepartmentDetailComponent = /** @class */ (function () {
-    function DepartmentDetailComponent() {
+    function DepartmentDetailComponent(route, router) {
+        this.route = route;
+        this.router = router;
     }
+    DepartmentDetailComponent.prototype.ngOnInit = function () {
+        // let id = this.route.snapshot.params['id'];
+        // this.departmentId = id;
+        var _this = this;
+        //params ob can detect any change in the route paramater
+        this.route.params.subscribe(function (params) {
+            var id = parseInt(params['id']);
+            _this.departmentId = id;
+        });
+    };
+    DepartmentDetailComponent.prototype.goPrevious = function () {
+        var previousId = this.departmentId - 1;
+        this.router.navigate(['/departments', previousId]);
+    };
+    DepartmentDetailComponent.prototype.goNext = function () {
+        var nextId = this.departmentId + 1;
+        this.router.navigate(['/departments', nextId]);
+    };
     DepartmentDetailComponent = __decorate([
         core_1.Component({
             selector: 'employee-detail',
-            template: "<h3>You Selected department with id = </h3>"
-        })
+            template: "<h3>You Selected department with id = {{departmentId}}</h3>\n             <a (click)=\"goPrevious()\">Previous</a>\n             <a (click)=\"goNext()\">Next</a>"
+        }),
+        __metadata("design:paramtypes", [router_1.ActivatedRoute, router_1.Router])
     ], DepartmentDetailComponent);
     return DepartmentDetailComponent;
 }());
--- app/department-detail.component.ts
+++ app/department-detail.component.ts
@@ -1,10 +1,35 @@
-import  { Component } from '@angular/core';
+import  { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,  Router, Params } from '@angular/router'
 
 
 @Component({
     selector:'employee-detail',
-    template:`<h3>You Selected department with id = </h3>`
+    template:`<h3>You Selected department with id = {{departmentId}}</h3>
+             <a (click)="goPrevious()">Previous</a>
+             <a (click)="goNext()">Next</a>`
 })
-export class DepartmentDetailComponent{
+export class DepartmentDetailComponent implements OnInit{
 
+    private departmentId:any;
+
+    constructor(private route:ActivatedRoute, private router:Router) {}
+
+    ngOnInit(){ 
+        // let id = this.route.snapshot.params['id'];
+        // this.departmentId = id;
+
+        //params ob can detect any change in the route paramater
+        this.route.params.subscribe((params:Params)=>{
+            let id = parseInt(params['id']);
+            this.departmentId = id;
+        })
+    }
+    goPrevious(){
+        let previousId = this.departmentId - 1;
+        this.router.navigate(['/departments', previousId]);
+    }
+    goNext(){
+        let nextId = this.departmentId + 1;
+        this.router.navigate(['/departments', nextId]);
+    }
 }
